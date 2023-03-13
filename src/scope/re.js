e1d60205298cd6d2dd3910b87c955175ae823d36
Christiaanvargas <56331682+christiaanvargas@users.noreply.github.com>
2023-03-12 21:56:39 -0500

    Declara, asignar, redeclarar y reasignar

---
+++ src/scope/re.js
@@ -0,0 +1,27 @@
+// var | declarar / asignar
+var firstName;  // Se asigna un valor por decfecto Undefined
+firstName = 'Oscar'; // se le asigna un valor de Oscar
+console.log(firstName);
+
+var lastName = 'David '; // declarar / asignar
+lastName = 'Anna'; // reasignar
+console.log(lastName);
+
+var secondName ='David'; // declarando / asignando
+var  secondName = 'Anna'; // reasignación
+console.log(secondName);
+
+// let | declarar / asignar
+let fruit = 'Apple'; // declarar / asignar
+fruit = 'Kiwi'; //reasignar
+console.log(fruit);
+
+let fruit = 'Banana';
+console.log(fruit);
+
+// const | declarar / asignar
+const animal = 'dog'; // declarar / asignar
+animal = 'cat'; // reasignar
+const = animal = 'snake';
+console.log(animal)
+ 
