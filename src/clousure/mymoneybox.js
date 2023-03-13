64fecc83e5b0066ee619d49bb7fc6e1eb8fa591b
Christiaanvargas <56331682+christiaanvargas@users.noreply.github.com>
2023-03-13 11:48:39 -0500

    lógica de Money Box

---
+++ src/clousure/mymoneybox.js
@@ -0,0 +1,29 @@
+/* 
+function moneyBox(coins) {
+    let saveCoins = 0;
+    saveCoins += coins;
+    console.log(`Money Box $ ${saveCoins}`);
+}
+
+moneyBox(5);
+moneyBox(5);
+*/
+
+function moneyBox() {
+    let saveCoins = 0;
+    function countCoins(coins) {
+        saveCoins += coins;
+        console.log(`Money Box $ ${saveCoins}`)
+    }
+    return countCoins;
+}
+
+const myMoneyBox = moneyBox();
+myMoneyBox(5);
+myMoneyBox(5);
+myMoneyBox(15);
+
+const moneyBoxAnna = moneyBox;
+moneyBoxAnna(10);
+moneyBoxAnna(20);
+moneyBoxAnna(5);
