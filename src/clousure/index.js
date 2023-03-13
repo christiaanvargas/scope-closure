a5754c59a53e2e133c9ba328cde818e79e166b00
Christiaanvargas <56331682+christiaanvargas@users.noreply.github.com>
2023-03-13 15:11:59 -0500

    Se crea el archivo index para explcar hoisting

---
+++ src/clousure/index.js
@@ -0,0 +1,16 @@
+// elevación de la variable nameOfDog
+
+// var nameOfDog; (valor asignado por defecto es undefined)
+/*
+console.log(nameOfDog);
+var nameOfDog ='Elmo';
+console.log(nameOfDog);
+*/
+
+nameOfDog ();
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${dog}`);
+}
+
+var dog = 'Elmo';
